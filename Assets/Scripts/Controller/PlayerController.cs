ad30f7e7d08b00992e054f3911001ff21b674fdf
Sehui Jeong <46295659+SayisMe@users.noreply.github.com>
2022-06-26 02:28:18 +0900

    Feat: 조이스틱으로 플레이어 움직임 + enemy 닿았을 때 데미지

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -6,21 +6,40 @@ public class PlayerController : MonoBehaviour
 {
     public int maxHP = 100;
     public int currentHP;
+    public float moveSpeed = 1.5f;
+
+    public Rigidbody2D rb; 
+    public Joystick joystick;
+
+    Vector2 movement;
 
     public UIManager uiManager;
-    // Start is called before the first frame update
+
     void Start()
     {
         currentHP = maxHP;
         uiManager.SetMaxHP(maxHP);
     }
 
-    // Update is called once per frame
+    //Input
     void Update()
     {
+        // movement.x = Input.GetAxisRaw("Horizontal");
+        // movement.y = Input.GetAxisRaw("Vertical");
+        movement.x = joystick.Horizontal;
+        movement.y = joystick.Vertical;
         if(Input.GetKeyDown(KeyCode.Space)) {
             Damage(10);
         }
+    }
+
+    //Movement
+    private void FixedUpdate() {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Damage(10);
     }
 
     void Damage(int damage) {

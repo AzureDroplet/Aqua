216ccb0d7f3fb414d4f22c549fc78b444b9b5f68
dmswl9898 <sejj0126@naver.com>
2022-06-26 18:43:35 +0900

    Feat : 아이템 파괴 함수 추가

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -9,22 +9,9 @@ public class Item : MonoBehaviour
         HPItem
     }
 
-    private ItemClass itemClass;
-
-    [Tooltip("scoreItem : 0, hpItem : 1")] 
-    public int itemClass;
     public int value;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Destroy(){
+        Destroy(this.gameObject);
     }
 }

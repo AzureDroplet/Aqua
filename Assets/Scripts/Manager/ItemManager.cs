faf9c4bfdadda679f9974561bca02f870e9e04b9
dmswl9898 <sejj0126@naver.com>
2022-06-26 18:44:13 +0900

    Feat : 아이템 생성 로직 추가

--- Assets/Scripts/Manager/ItemManager.cs
+++ Assets/Scripts/Manager/ItemManager.cs
@@ -9,23 +9,39 @@ public class ItemManager : MonoBehaviour
     public GameObject[] hearts;
     public GameObject[] trashes;
 
-    public void Create(){
+    public int buildY = -5;
 
+    void Start()
+    {
+        StartCoroutine(CreateItem());
     }
 
-    public void Destroy(){
+    IEnumerator CreateItem(){
+        while(true){
+            yield return new WaitForEndOfFrame();
+            if(buildY > 10) continue; //플레이어랑 너무 멀리 떨어져있으면 넘김
 
+            int r = Random.Range(0,100);
+            if(r < 20) {
+                GameObject i = Instantiate(coins[Random.Range(0,coins.Length)], new Vector2(GetCreatePosX(), buildY), Quaternion.identity) as GameObject;
+            }
+            else if(r < 30) {
+                GameObject i = Instantiate(hearts[Random.Range(0,hearts.Length)], new Vector2(GetCreatePosX(), buildY), Quaternion.identity) as GameObject;
+            }
+            else if(r < 40) {
+                GameObject i = Instantiate(trashes[Random.Range(0,trashes.Length)],new Vector2(GetCreatePosX(), buildY), Quaternion.identity) as GameObject;
+            }
+            buildY++;
+        }
     }
 
     public int GetItemValue(GameObject item){
-        item _item = item.GetComponent<item>();
-        const int itemValue = _item.value;
-        return itemValue;
+        Item _item = item.GetComponent<Item>();
+        return _item.value;
     }
 
-    public int GetItemClass(GameObject item){
-        item _item = item.GetComponent<item>();
-        const int itemClass = _item.itemClass;
-        return itemClass;       
+    public float GetCreatePosX(){
+        float x = Random.Range(-5.0f, 5.0f);
+        return x;
     }
 }

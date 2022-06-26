6733322e8be56b7b948df898af0dba687ce2a1fe
Sehui Jeong <46295659+SayisMe@users.noreply.github.com>
2022-06-26 23:58:45 +0900

    Feat: hpBar 색상적용

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public Slider hpBar;
+    public Gradient gradient;
+    public Image fill;
     // Singleton 처리
     public static UIManager Instance;
     private void Awake()
@@ -16,9 +18,12 @@ public class UIManager : MonoBehaviour
     public void SetMaxHP(int hp) {
         hpBar.maxValue = hp;
         hpBar.value = hp;
+        // slider 1값일 때의 gradient 색으로 설정
+        fill.color = gradient.Evaluate(1f);
     }
 
     public void SetHP(int hp) {
         hpBar.value = hp;
+        fill.color = gradient.Evaluate(hpBar.normalizedValue);
     }
 }

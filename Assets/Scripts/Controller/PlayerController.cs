17ed8fd6092fab6bda612d43f0485c3015a70e78
Sehui Jeong <46295659+SayisMe@users.noreply.github.com>
2022-06-28 18:03:19 +0900

    Feat: 적 충돌 시 2초간 무적상태 전환

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -7,9 +7,13 @@ public class PlayerController : MonoBehaviour
     public int maxHP = 100;
     public int currentHP;
     public float moveSpeed = 1.5f;
+    public bool isUnBeatTime = false;
+    public float unBeatTime = 2; //2초동안 깜빡임
 
     public Rigidbody2D rb; 
     public Joystick joystick;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2D;
 
     Vector2 movement;
 
@@ -17,6 +21,8 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        boxCollider2D = gameObject.GetComponent<BoxCollider2D>();
         currentHP = maxHP;
         uiManager.SetMaxHP(maxHP);
     }
@@ -45,5 +51,28 @@ public class PlayerController : MonoBehaviour
     void Damage(int damage) {
         currentHP -= damage;
         uiManager.SetHP(currentHP);
+        isUnBeatTime = true;
+        boxCollider2D.enabled = false;
+        StartCoroutine("UnBeatTime");
+    }
+
+    IEnumerator UnBeatTime()
+    {
+        for(int i = 0; i < unBeatTime * 5; ++i)
+        {
+            if(i % 2 == 0)
+                spriteRenderer.color = new Color32(255, 255, 255, 90);
+            else
+                spriteRenderer.color = new Color32(255, 255, 255, 180);
+
+            yield return new WaitForSeconds(0.2f);
+        }
+
+        //Alpha Effect End
+        spriteRenderer.color = new Color32(255, 255, 255, 255);
+        boxCollider2D.enabled = true;
+        isUnBeatTime = false;
+
+        yield return null;
     }
 }

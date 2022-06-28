560c236da6d9aba27a8d7f00ad81e8d820f34c02
dmswl9898 <sejj0126@naver.com>
2022-06-28 19:12:35 +0900

    Feat : 싱글톤 추가 및 UI 관련 함수 추가

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -10,17 +10,44 @@ public class GameManager : MonoBehaviour
         Over
     }
 
-    GameState _curGameState = GameState.Ready;
+    public static GameManager instance = null;
 
+    private void Awake()
+    {
+        if (instance == null) 
+        {
+            instance = this; 
+            DontDestroyOnLoad(gameObject); 
+        }
+        else
+        {
+            if (instance != this)
+                Destroy(this.gameObject); 
+        }
+
+    }
+    GameState _curGameState = GameState.Ready;
+    UIManager _uiManager;
+
+    Start(){
+        _uiManager = UIManager.Instance;
+    }
     void SetGameState(GameState gameState){
         _curGameState = gameState;
 
-        switch(_curGameState){
-            case GameState.Ready : Debug.Log("게임 준비"); break;
+        switch (_curGameState)
+        {
+            case GameState.Ready:
+                Debug.Log("게임 준비");
+                _uiManager.SetReadyUI(); break;
 
-            case GameState.Start : Debug.Log("게임 시작");  break;
+            case GameState.Start:
+                Debug.Log("게임 시작");
+                _uiManager.SetStartUI(); break;
 
-            case GameState.Over : Debug.Log("게임 오버");  break;
+            case GameState.Over:
+                Debug.Log("게임 오버");
+                _uiManager.SetEndUI(); break;
         }
     }
 

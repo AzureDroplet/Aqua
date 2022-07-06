0b2ff3f848d39ace022796ba94270c9708fdb39e
dmswl9898 <sejj0126@naver.com>
2022-07-06 21:37:02 +0900

    Feat : Player Score 설정

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 1.5f;
     public bool isUnBeatTime = false;
     public float unBeatTime = 2; //2초동안 깜빡임
+    public int score = 0;
+    int _maxPos = 0;
 
     public Rigidbody2D rb; 
     public Joystick joystick;
@@ -25,6 +27,8 @@ public class PlayerController : MonoBehaviour
         boxCollider2D = gameObject.GetComponent<BoxCollider2D>();
         currentHP = maxHP;
         uiManager.SetMaxHP(maxHP);
+
+        StartCoroutine("SetBasicScore");
     }
 
     //Input
@@ -49,7 +53,14 @@ public class PlayerController : MonoBehaviour
             Damage(10);
         }
         else if(collider.tag == "Item") {
-
+            string itemName = collider.gameObject.name;
+            int value = collider.gameObject.transform.GetComponent<Item>().value;
+            if(itemName.Contains("trash"))
+                ScoreDown(value);
+            else if(itemName.Contains("coin"))
+                ScoreUp(value);
+            else if(itemName.Contains("heart"))
+                HpUp(value);
         }
     }
 
@@ -79,5 +90,32 @@ public class PlayerController : MonoBehaviour
         isUnBeatTime = false;
 
         yield return null;
+    }
+
+    void ScoreUp(int value){
+        score += value;
+        uiManager.SetScore(score);
+    }
+
+    void ScoreDown(int value){
+        score -= value;
+        uiManager.SetHP(score);
+    }
+
+    void HpUp(int value){
+        currentHP += value;
+        uiManager.SetHP(currentHP);
+    }
+    
+    IEnumerator PositionScore(){
+        while(true){
+            yield return null;
+            int pos = Mathf.RoundToInt(this.transform.position.y);
+            if(pos == _maxPos) break;
+            _maxPos = (_maxPos < pos) ? pos : _maxPos;
+            if(_maxPos % 10 == 0){
+                ScoreUp(20);
+            }
+        }
     }
 }

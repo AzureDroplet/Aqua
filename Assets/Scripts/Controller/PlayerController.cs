aa82c0fcb9009341981eecc104d80aba60c41408
Sehui Jeong <46295659+SayisMe@users.noreply.github.com>
2023-01-02 17:11:54 +0900

    Feat: 입력한 nickname 대로 서버랭킹등록

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -70,7 +70,7 @@ public class PlayerController : MonoBehaviour
         currentHP -= damage;
         if(currentHP <= 0) {
             GameManager.instance.SetGameState("End");
-            ServerInput.instance.AddRank("aqua", score);
+            ServerInput.instance.AddRank(uiManager.textNickname.text, score);
         }
         uiManager.SetHP(currentHP);
         isUnBeatTime = true;
